2baf4dfd9ec9301ccb322b909dee155237e1acff
Wouter Pen <penwouter@gmail.com>
2018-10-08 15:24:04 +0200

    first commit

---
+++ practice3.js
@@ -0,0 +1,42 @@
+
+function JScriptVoorbeeld () {
+}
+
+
+function sayHello(name){
+    const value = document.getElementById(name).getAttribute("value")
+    if(value != null){
+        alert("hello "+ value);
+    } else {
+        alert("hello mister or misses unknown");
+    };
+};
+
+function forLoop(){
+    for(var i = 0 ; i<5; i++){
+            console.log(i);
+        }
+    };
+
+function Count(event){
+    console.log(event);
+    case
+};
+
+function countLines(){
+    const Lines = document.getElementById('lineCount').value;
+    
+    for(var i = 0 ; i<Lines; i++){
+        
+        var btn = document.createElement("BUTTON");        // Create a <button> element
+        var t = document.createTextNode(i+1);
+        btn.id = "ID" + i;
+        btn.className = i;
+        btn.style.color = i%2 ? 'green': 'red';
+        btn.appendChild(t);
+        document.body.appendChild(btn);
+    }
+};
+
+
+
